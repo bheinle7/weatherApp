fa5bef1b0f4ee0a8b7ff0f7cdbe3ccb1124b71c2
Benjamin Heinle <bheinle7@gmail.com>
2017-06-01 15:12:28 -0500

    Basic HTML, JS, and CSS for Site

--- index.js
+++ index.js
@@ -1,27 +1,117 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    $.ajax({
-      url: "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&key=AIzaSyBhjujJO7PSaHpsG4Ab9Y-iPQZb0i9czio",
-      dataType: "json",
-      success: function(locationData) {
-      $("#cityState").html(locationData.results[0].address_components[2].short_name+", "+locationData.results[0].address_components[4].short_name);
-      }
+$(document).ready(function() {
+  var temp;
+  var hi;
+  var lo;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+      $.ajax({
+        url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+          lat +
+          "," +
+          lon +
+          "&key=AIzaSyBhjujJO7PSaHpsG4Ab9Y-iPQZb0i9czio",
+        dataType: "json",
+        success: function(locationData) {
+          $("#cityState").html(
+            locationData.results[0].address_components[2].short_name +
+              ", " +
+              locationData.results[0].address_components[4].short_name
+          );
+        }
+      });
+      $.ajax({
+        url: "https://api.darksky.net/forecast/e7f8b6c907efe5aacff9b70d8b6ab98c/" +
+          lat +
+          "," +
+          lon,
+        dataType: "jsonp",
+        success: function(weatherData) {
+          $("#crntCond").html(weatherData.currently.summary);
+          temp = Math.round(weatherData.currently.temperature);
+          $("#temp").html(
+            Math.round(weatherData.currently.temperature) + "\xB0F"
+          );
+          hi = Math.round(weatherData.daily.data[0].temperatureMax);
+          $("#hiT").html(
+            "Hi " +
+              Math.round(weatherData.daily.data[0].temperatureMax) +
+              "\xB0\u2191"
+          );
+          lo = Math.round(weatherData.daily.data[0].temperatureMin);
+          $("#loT").html(
+            "Lo " +
+              Math.round(weatherData.daily.data[0].temperatureMin) +
+              "\xB0\u2193"
+          );
+          var icon = weatherData.daily.data[0].icon
+          switch (icon) {
+            case "clear-day":
+              $("#icon").attr("class", "wi wi-day-sunny");
+              $("body").css("background-image", "url("+"http://i38.photobucket.com/albums/e141/bheinle7/clearDay_zpsm1ukvbtm.jpeg"+")");
+              break;
+            case "clear-night":
+              $("#icon").attr("class", "wi wi-night-clear");
+              $("body").css("background-image", "url("+"http://i38.photobucket.com/albums/e141/bheinle7/clearNight_zpshczgiraa.jpg"+")");
+              break;
+            case "rain":
+              $("#icon").attr("class", "wi wi-sprinkle");
+              $("body").css("background-image", "url("+"http://i38.photobucket.com/albums/e141/bheinle7/rainyDay_zpsyxufjapo.jpeg"+")");
+              break;
+            case "snow":
+              $("#icon").attr("class", "wi wi-snow");
+              $("body").css("background-image", "url("+"http://i38.photobucket.com/albums/e141/bheinle7/snow_zpsneddlu7w.jpg"+")");
+              break;
+            case "sleet":
+              $("#icon").attr("class", "wi wi-sleet");
+              $("body").css("background-image", "url("+"http://i38.photobucket.com/albums/e141/bheinle7/rainyDay_zpsyxufjapo.jpeg"+")");
+              break;
+            case "wind":
+              $("#icon").attr("class", "wi wi-strong-wind");
+              $("#icon").css("color", "black");
+              $("body").css("color", "black");
+              $("body").css("background-image", "url("+"http://i38.photobucket.com/albums/e141/bheinle7/windy_zps1ro7flpo.jpg"+")");
+              break;
+            case "fog":
+              $("#icon").attr("class", "wi wi-fog");
+              $("#icon").css("color", "black");
+              $("body").css("color", "black");
+              $("a").css("color", "black");
+              $("body").css("background-image", "url("+"http://i38.photobucket.com/albums/e141/bheinle7/foggyDay_zpsaj0cwn3l.jpeg"+")");
+              break;
+            case "cloudy":
+              $("#icon").attr("class", "wi wi-cloud");
+              $("body").css("background-image", "url("+"http://i38.photobucket.com/albums/e141/bheinle7/cloudy_zps4ne7cp5a.jpeg"+")");
+              break;
+            case "partly-cloudy-day":
+              $("#icon").attr("class", "wi wi-day-cloudy");
+              $("#icon").css("color", "black");
+              $("body").css("color", "black");
+              $("a").css("color", "black");
+              $("body").css("background-image", "url("+"http://i38.photobucket.com/albums/e141/bheinle7/partlyCloudy_zpsuein9br9.jpeg"+")");
+              break;
+            case "partly-cloudy-night":
+              $("#icon").attr("class", "wi wi-night-alt-cloudy");
+              $("body").css("background-image", "url("+"http://i38.photobucket.com/albums/e141/bheinle7/partlyCloudyNight_zpsx7nw2kh1.jpg"+")");
+              break;
+          }
+        }
+      });
     });
-    $.ajax({
-      url: "https://api.darksky.net/forecast/e7f8b6c907efe5aacff9b70d8b6ab98c/"+lat+","+lon,
-      dataType: "jsonp",
-      success: function(weatherData) {
-        $("#crntCond").html(weatherData.currently.summary)
-        $("#temp").html(Math.round(weatherData.currently.temperature)+"\xB0")
-        $("#hiT").html("Hi "+Math.round(weatherData.daily.data[0].temperatureMax)+"\xB0\u2191")
-        $("#loT").html("Lo "+Math.round(weatherData.daily.data[0].temperatureMin)+"\xB0\u2193")
-        //alert(weatherData.daily.data[0].icon)
-        var skycons = new Skycons({"color": "black"});
-        skycons.add("icon1", Skycons.PARTLY_CLOUDY_DAY);
-        skycons.play();
-      }
+  }
+  $("#degC").click(function(){
+        $("#degF").attr("class","btn btn-default");
+        $("#degC").attr("class","btn btn-primary");
+        $("#temp").html(Math.round((temp-32)*(5/9)) + "\xB0C")
+        $("#hiT").html("Hi " + Math.round((hi-32)*(5/9)) + "\xB0\u2191")
+        $("#loT").html("Lo " + Math.round((lo-32)*(5/9)) + "\xB0\u2193")
     });
-  });
-}
+  $("#degF").click(function(){
+        $("#degC").attr("class","btn btn-default");
+        $("#degF").attr("class","btn btn-primary");
+        $("#temp").html(temp + "\xB0F")
+        $("#hiT").html("Hi " + hi + "\xB0\u2191")
+        $("#loT").html("Lo " + lo + "\xB0\u2193")
+    });
+});
